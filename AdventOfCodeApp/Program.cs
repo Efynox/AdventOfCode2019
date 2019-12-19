0d68e266ee8e2e9e802201c472358137959f6ff5
Efynox <efynox@live.fr>
2019-12-19 23:31:18 +0100

    Day 3

--- AdventOfCodeApp/Program.cs
+++ AdventOfCodeApp/Program.cs
@@ -9,11 +9,9 @@ namespace AdventOfCodeApp
         {
             Console.Write("Day: ");
 
-            int day;
-            if (!int.TryParse(Console.ReadLine(), out day))
-                throw new Exception("Error: Invalid day format");
+            string day = Console.ReadLine();
 
-            var type = typeof(Program).Assembly.GetType(string.Format("AdventOfCodeApp.Day{0}.Main", day.ToString().PadLeft(2, '0')));
+            var type = typeof(Program).Assembly.GetType(string.Format("AdventOfCodeApp.Day{0}.Main", day.PadLeft(2, '0')));
             if (type == null)
                 throw new Exception("Error: Day not found");
 
